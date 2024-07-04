a3047ed952a6233dcefade823a263b477846a171
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-07-04 22:46:59 +0200

    feat: function

---
+++ go/lasagna-master/lasagna_master.go
@@ -0,0 +1,23 @@
+package lasagna
+
+func PreparationTime(layers []string, time int) int {
+	if time == 0 {
+		time = 2
+	}
+	return len(layers) * time
+}
+
+// TODO: define the 'Quantities()' function
+
+// TODO: define the 'AddSecretIngredient()' function
+
+// TODO: define the 'ScaleRecipe()' function
+
+// Your first steps could be to read through the tasks, and create
+// these functions with their correct parameter lists and return types.
+// The function body only needs to contain `panic("")`.
+// 
+// This will make the tests compile, but they will fail.
+// You can then implement the function logic one by one and see
+// an increasing number of tests passing as you implement more 
+// functionality.
