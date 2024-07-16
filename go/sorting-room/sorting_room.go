139ba0b6138741a4511dea7504b55d7b471a94a5
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-07-16 21:59:44 +0200

    feat: function call

--- go/sorting-room/sorting_room.go
+++ go/sorting-room/sorting_room.go
@@ -47,7 +47,7 @@ func ExtractFancyNumber(fnb FancyNumberBox) int {
 
 // DescribeFancyNumberBox should return a string describing the FancyNumberBox.
 func DescribeFancyNumberBox(fnb FancyNumberBox) string {
-	panic("Please implement DescribeFancyNumberBox")
+	return fmt.Sprintf("This is a fancy box containing the number %.1f", float32(ExtractFancyNumber(fnb)))
 }
 
 // DescribeAnything should return a string describing whatever it contains.
