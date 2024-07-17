7eb0fdd1a2660512578fab153d3de38cadd0f367
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-07-17 09:13:23 +0200

    feat: loop in range

---
+++ go/chessboard/chessboard.go
@@ -0,0 +1,35 @@
+package chessboard
+
+// File stores if a square is occupied by a piece - this will be a slice of bools
+type File []bool
+
+// Chessboard contains a map of eight Files, accessed with keys from "A" to "H"
+type Chessboard map[string]File
+
+// CountInFile returns how many squares are occupied in the chessboard,
+// within the given file.
+func CountInFile(cb Chessboard, file string) int {
+	occupied := 0
+	for _, square := range cb[file] {
+		if square {
+			occupied += 1
+		}
+	}
+	return occupied
+}
+
+// CountInRank returns how many squares are occupied in the chessboard,
+// within the given rank.
+func CountInRank(cb Chessboard, rank int) int {
+	panic("Please implement CountInRank()")
+}
+
+// CountAll should count how many squares are present in the chessboard.
+func CountAll(cb Chessboard) int {
+	panic("Please implement CountAll()")
+}
+
+// CountOccupied returns how many squares are occupied in the chessboard.
+func CountOccupied(cb Chessboard) int {
+	panic("Please implement CountOccupied()")
+}
