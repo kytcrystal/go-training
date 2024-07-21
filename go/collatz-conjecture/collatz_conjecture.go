59a71eb898587ccdc0ec540c78962ca8f2816fbd
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-07-21 11:07:12 +0200

    tdd2: error cases

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,10 @@
 package collatzconjecture
 
+import "errors"
+
 func CollatzConjecture(n int) (int, error) {
+	if n <= 0 {
+		return 0, errors.New("zero or negative value is not allowed")
+	}
 	return 0, nil
 }
