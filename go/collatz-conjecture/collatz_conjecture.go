45b3d6aa9366e7f84ec8c9488e07165d1ca2d124
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-07-22 11:15:12 +0200

    tdd5: refactor

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -6,17 +6,17 @@ func CollatzConjecture(n int) (int, error) {
 	if n <= 0 {
 		return 0, errors.New("zero or negative value is not allowed")
 	}
+	
 	steps := 0
 	for n > 1 {
 		if n % 2 == 0 {
 			n /= 2
-			steps += 1
 		} else {
 			n *= 3
 			n += 1
-			steps += 1
 		}
+		steps += 1
 	}
-	
+
 	return steps, nil
 }
