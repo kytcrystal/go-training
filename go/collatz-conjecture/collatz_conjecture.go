6f18607d9b61a4027952b2b956f640ea31137e3e
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-07-22 11:13:21 +0200

    tdd3: even case

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -6,5 +6,15 @@ func CollatzConjecture(n int) (int, error) {
 	if n <= 0 {
 		return 0, errors.New("zero or negative value is not allowed")
 	}
-	return 0, nil
+	steps := 0
+	for n > 1 {
+		if n % 2 == 0 {
+			n /= 2
+			steps += 1
+		} else {
+			return steps, nil
+		}
+	}
+	
+	return steps, nil
 }
