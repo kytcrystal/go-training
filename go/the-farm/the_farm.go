e986d408488adf38a5e8c2b09c320a39f08519da
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-07-22 11:24:31 +0200

    tdd1: compile code

--- go/the-farm/the_farm.go
+++ go/the-farm/the_farm.go
@@ -1,16 +1,15 @@
 package thefarm
 
 // TODO: define the 'DivideFood' function
+func DivideFood(fc FodderCalculator, nc int) (float64, error) {
+	return 0.0, nil
+}
 
 // TODO: define the 'ValidateInputAndDivideFood' function
-
+func ValidateInputAndDivideFood(fc FodderCalculator, nc int) (float64, error) {
+	return 0.0, nil
+}
 // TODO: define the 'ValidateNumberOfCows' function
-
-// Your first steps could be to read through the tasks, and create
-// these functions with their correct parameter lists and return types.
-// The function body only needs to contain `panic("")`.
-//
-// This will make the tests compile, but they will fail.
-// You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more
-// functionality.
+func ValidateNumberOfCows(nc int) error {
+	return nil
+}
