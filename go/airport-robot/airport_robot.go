ecd1d023effbf0ced14657239577e73377b393eb
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-07-28 22:51:46 +0200

    feat: greeter func

--- go/airport-robot/airport_robot.go
+++ go/airport-robot/airport_robot.go
@@ -3,3 +3,12 @@ package airportrobot
 // Write your code here.
 // This exercise does not have tests for each individual task.
 // Try to solve all the tasks first before running the tests.
+
+type Greeter interface {
+	LanguageName(language string) string
+	Greet(name string) string
+}
+
+func SayHello(name string, g Greeter) string {
+	return g.Greet(name)
+}
